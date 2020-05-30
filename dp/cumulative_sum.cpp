ab43576b39bc2248864794a51daec3073af403f5
naotaka nakane <n.nakane0219@gmail.com>
2020-05-31 07:50:40 +0900

    Update cumulative_sum

--- dp/cumulative_sum.cpp
+++ dp/cumulative_sum.cpp
@@ -1,19 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+template<typename T>
 struct CumulativeSum {
-    int n;
-    vector<long long> nums;
-    vector<long long> s;
-    CumulativeSum(int n) : n(n), s(n + 1, 0) { }
-    void add(long long num) {
-        nums.push_back(num);
-    }
-    void build() {
-        for (int i = 0; i < n; ++i) s[i+1] = s[i] + nums[i];
+    vector<T> s;
+    CumulativeSum(const vector<T> &nums) {
+        int n = nums.size();
+        s.resize(n + 1);
+        s[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            s[i + 1] = s[i] + nums[i];
+        }
     }
     // [l, r), l and r is 0-indexed.
-    long long sum(int l, int r) {
+    T sum(int l, int r) {
         return s[r] - s[l];
     }
 };
@@ -26,13 +26,10 @@ int main() {
     vector< long long > ress;
     while (cin >> n >> k) {
         if (n == 0) break;
-        CumulativeSum cs(n);
-        for (int i = 0; i < n; ++i) {
-            int a; cin >> a;
-            cs.add(a);
-        }
-        cs.build();
-        long long res = -1LL<<60;
+        vector<long long> nums(n);
+        for (int i = 0; i < n; ++i) cin >> nums[i];
+        CumulativeSum<long long> cs(nums);
+        long long res = numeric_limits<long long>::min();
         for (int i = 0; i <= n - k; ++i) {
             long long val = cs.sum(i, i + k);
             res = max(res, val);
