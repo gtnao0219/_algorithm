e6164aaeb89d5f9e2be2ed9a92e4fdab3bf92b20
naotaka nakane <n.nakane0219@gmail.com>
2020-06-09 01:12:18 +0900

    Update UnionFindTree

--- data_structure/union_find_tree.cpp
+++ data_structure/union_find_tree.cpp
@@ -2,35 +2,36 @@
 using namespace std;
 
 struct UnionFindTree {
-    vector<int> par, rank;
-    UnionFindTree(int n) : par(n , -1), rank(n, 0) { }
-    int find(int x) {
-        if (par[x] == -1) return x;
-        else return par[x] = find(par[x]);
+    UnionFindTree(int n) : parent(n, -1), rank(n, 0) { }
+    int root(int x) {
+        if (parent[x] == -1) return x;
+        else return parent[x] = root(parent[x]);
     }
     void unite(int x, int y) {
-        x = find(x);
-        y = find(y);
+        x = root(x);
+        y = root(y);
         if (x == y) return;
         if (rank[x] < rank[y]) swap(x, y);
         if (rank[x] == rank[y]) ++rank[x];
-        par[y] = x;
+        parent[y] = x;
     }
     bool same(int x, int y) {
-        return find(x) == find(y);
+        return root(x) == root(y);
     }
+private:
+    vector<int> parent, rank;
 };
 
 /*
     verified
-    https://atcoder.jp/contests/abc120/tasks/abc120_d
+    https://atcoder.jp/contests/atc001/tasks/unionfind_a
 */
 int main() {
     int n, q; cin >> n >> q;
-    UnionFindTree union_find_tree(n);
+    UnionFindTree uft(n);
     for (int i = 0; i < q; ++i) {
         int com, x, y; cin >> com >> x >> y;
-        if (com == 0) union_find_tree.unite(x, y);
-        else cout << union_find_tree.same(x, y) << endl;
+        if (com == 0) uft.unite(x, y);
+        else cout << (uft.same(x, y) ? "Yes" : "No") << endl;
     }
 }
